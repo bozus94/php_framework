e0b2b016b93e6c231e6adb81a9c66da138c33e58
bozus94 <bj_ortega@hotmail.com>
2021-06-21 16:07:23 +0200

    corregido error de sintaxis en la vista register >>* Trabajando en la seccion de migraciones

--- resources/views/auth/register.php
+++ resources/views/auth/register.php
@@ -11,7 +11,7 @@ $form = Form::begin('/register', 'POST', 'row g-3') ?>
 <div class="col-6"><?= $form->field(($model) ?? new RegisterModel, 'Apellido', 'lastName', 'text'); ?></div>
 <div class="col-6"><?= $form->field(($model) ?? new RegisterModel, 'Correo electronico', 'email', 'email'); ?></div>
 <div class="col-6"><?= $form->field(($model) ?? new RegisterModel, 'Usuario', 'user', 'user'); ?></div>
-<div class="col-12"><?= $form->field(($model) ?? new RegisterModel, 'Contraeña', 'password', 'password'); ?></div>
+<div class="col-12"><?= $form->field(($model) ?? new RegisterModel, 'Contraseña', 'password', 'password'); ?></div>
 <div class="col-12"><?= $form->field(($model) ?? new RegisterModel, 'Confirmar la contraseña', 'confirmPassword', 'password'); ?></div>
 <div class="col-12"><?= $form->check(($model) ?? new RegisterModel, 'Acepto los terminos', 'terms'); ?></div>
 <div class="col-12"><?= $form->submit('Enviar'); ?></div>
